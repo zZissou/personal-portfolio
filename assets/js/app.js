edb12ca6f582aefba01ab9f85a31ba7587a3f0ad
zZissou <kyle.edward.russell@gmail.com>
2016-10-28 15:44:21 -0700

    cleaned up extra html files and duplicate css file.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,21 @@
 // sanity check
 console.log("app.js linked.");
+
+$(document).ready(function() {
+  //change the integers below to match the height of your upper dive, which I called
+  //banner.  Just add a 1 to the last number.  console.log($(window).scrollTop())
+  //to figure out what the scroll position is when exactly you want to fix the nav
+  //bar or div or whatever.  I stuck in the console.log for you.  Just remove when
+  //you know the position.
+  $(window).scroll(function () {
+
+    console.log($(window).scrollTop());
+
+    if ($(window).scrollTop() < 485) {
+      $('#nav_bar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() > 486) {
+      $('#nav_bar').removeClass('navbar-fixed');
+    }
+  });
+});
